90f98eb120f409e0924f4c4cb3093321b6669f86
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-08-18 16:05:41 -0400

    Done

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function theTruth() {
+  return true;
+}
+
+function favoriteIceCream(ic) {
+  return "I love " + ic;
+}
+
+function shouting(line) {
+  return line.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
