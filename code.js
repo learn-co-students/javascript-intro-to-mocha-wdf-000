243b4dfc0ea589b3735729fcecbbee1fe9da5d06
blessedsoy <unmigong@gmail.com>
2016-08-24 14:45:14 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function favoriteIceCream(fic) {
+   return `I love ${fic}`;
+ }
+
+ function shouting(phrase) {
+   return phrase.toUpperCase();
+
+ }
+
+ function roundDown(num) {
+   return Math.floor(num);
+ }
+
+ function theTruth() {
+   return true
+ }
