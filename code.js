32ea920892f863c1bb5bca33bb49ee412c77c550
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-02 21:39:13 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favoriteIceCream(name) {
+  return `I love ${name}`;
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function shouting(sentence) {
+  return sentence.toUpperCase();
+}
+
+function theTruth() {
+  return true;
+}
