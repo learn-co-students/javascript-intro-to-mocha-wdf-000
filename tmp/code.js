b4763fbeb796b626f6e9c82ce30bf91113915cc1
Raycent <raycent.tan@flatironschool.com>
2016-09-06 11:34:36 -0400

    Done.

---
+++ tmp/code.js
@@ -0,0 +1,15 @@
+function favoriteIceCream(name) {
+  return `I love ${name}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
