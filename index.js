8586b42b93f1addb094c42cfa4228e9e6348fee9
Flatiron School <github email address>
2016-10-19 16:49:02 -0400

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return "Hey, " + name + "!";
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(words) {
+  return words.toUpperCase();
+}
+
+
+function roundDown(num) {
+  return Math.floor(num);
+}
