f77c912c6403fae8a21bcd1c0716358c6f25c691
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-23 12:00:15 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
