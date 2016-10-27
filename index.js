8ff39c31a4a4976381ede176f5da6fdeac0d779c
Bibi Khadiza <limakhadiza@yahoo.com>
2016-10-27 13:05:13 -0400

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+ return "Hey!";
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(cream){
+  return `I love ${cream}`;
+}
+
+function shouting(shout){
+  return shout.toUpperCase();
+}
+
+function roundDown(decimal){
+  return Math.floor(decimal);
+}
