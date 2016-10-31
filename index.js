67627b22d057c5326ac0e034fc8503a59ed1b542
Naveed Shah <naveed@tayybah.com>
2016-10-31 14:44:11 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return 'Hey' + name + '!';
+}
+
+function favoriteIceCream(icecream) {
+  return 'I love ' + icecream
+}
+
+function shouting(string) {
+ return string.toUpperCase();
+}
+
+function roundDown(num) {
+  return num.floor();
+}
