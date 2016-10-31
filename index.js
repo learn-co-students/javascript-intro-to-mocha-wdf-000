23642113e49f3eb07e20866329b9e13b4cbeb3f7
Isuru <github email address>
2016-10-31 14:55:20 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(line){
+  return `${line.toUpperCase()}`
+}
+
+function roundDown(number){
+  return Math.floor(number)
+}
