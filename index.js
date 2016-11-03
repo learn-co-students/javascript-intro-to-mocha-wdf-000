3b2b8ca1e5e95d82236a772da32df87837b46e5d
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-11-03 14:52:52 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(ice) {
+  return `I love ${ice}`;
+}
+
+function shouting() {
+  return "I LOVE JAVASCRIPT SOOOOOO MUCH";
+}
+
+function roundDown(numb) {
+  return Math.floor(numb);
+}
