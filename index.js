b54656b06e610d5e74d002dfdac2dd3f68b26d06
parm530 <parmsahadeo530@gmail.com>
2016-11-03 17:41:56 -0400

    Done

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return "Hey!";
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(icecream){
+  return `I love ${icecream}`;
+}
+
+function shouting(string){
+  var string2 = string.toUpperCase();
+  return string2;
+}
+
+function roundDown(num){
+  var num2 = Math.floor(num);
+  return num2;
+}
