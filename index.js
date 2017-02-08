b8d79ceb25c752f4b3870f95418096e078273111
Ekaterina Petushova <epetushova@gmail.com>
2017-02-08 13:18:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,83 @@
-function favoriteIceCream(flavor) {
-	return "I love " + flavor
+function theTruth(){
+	return true;
 }
 
-function theTruth() {
-  return true;
-}
-
-function sayHey(){
-	return 'Hey!';
+function  sayHey(){
+	return "Hey!";
 }
 
 function sayHeyFriend(name){
-	return 'Hey, ' + name + '!';
+	return `Hey, ${name}!`;
 }
 
-function shouting(string){
-	return string.toUpperCase();
+function favoriteIceCream(flavor){
+	return `I love ${flavor}`;
+}
+
+function shouting(phrase){
+	return phrase.toUpperCase();
 }
 
 function roundDown(number){
 	return Math.floor(number);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function favoriteIceCream(flavor) {
+// 	return "I love " + flavor
+// }
+
+// function theTruth() {
+//   return true;
+// }
+
+// function sayHey(){
+// 	return 'Hey!';
+// }
+
+// function sayHeyFriend(name){
+// 	return 'Hey, ' + name + '!';
+// }
+
+// function shouting(string){
+// 	return string.toUpperCase();
+// }
+
+// function roundDown(number){
+// 	return Math.floor(number);
+// }
